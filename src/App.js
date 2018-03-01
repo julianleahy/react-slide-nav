3526cd55755fb8d73b348f5a0ff222f774d29a6b
julianleahy <julianleahynz@gmail.com>
2018-03-01 15:04:14 +1300

    add a link to github

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
+import GitLink from './GitLink/GitLink';
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
+            <div>
+                <GitLink />
                 <Navbar />
+            </div>
             </BrowserRouter>
         );
     }
---
+++ src/GitLink/GitLink.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './GitLink.css'
+
+const GitLink = (props) => (
+  <div className="GitWrap">
+    <div className="GitLogo">
+      <i className="fab fa-github"></i>
+    </div>
+    <div className="GitHub">
+      <a href="https://github.com/julianleahy/react-slide-nav">View on GitHub</a>
+    </div>
+  </div>
+)
+
+export default GitLink;
