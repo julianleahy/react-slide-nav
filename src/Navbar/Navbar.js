29eb10c58d95bac65fa09b10aec215aa1bac8a69
julianleahy <julianleahynz@gmail.com>
2018-03-01 18:38:20 +1300

    change icon button to a full width navbar and add site title

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -28,29 +28,36 @@ class Navbar extends Component {
 
         return (
             <Aux>
-                <div className={menuBtnClass.join(' ')} onClick={this.toggleMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <nav className={navbarClass.join(' ')}>
-                    <ul className="NavbarMenu" onClick={this.toggleMenu}>
-                        <li><NavLink to='/' exact >Home</NavLink></li>
-                        <li><NavLink to='/about' exact >About</NavLink></li>
-                        <li><NavLink to='/contact' exact >Contact</NavLink></li>
-                    </ul>
-                    <ul className="NavbarSocialIcon">
-                    <li><a href=""><i className="fab fa-facebook-f"></i></a></li>
-                    <li><a href=""><i className="fab fa-twitter"></i></a></li>
-                    <li><a href=""><i className="fab fa-github"></i></a></li>   
-                </ul>
-                </nav>
-                
-    
+                <div className='NavWrap'>
+                <p className='NavTitle'>julianleahy.com</p>
+                    <div className={menuBtnClass.join(' ')} onClick={this.toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <nav className={navbarClass.join(' ')}>
+                        <ul className="NavbarMenu" onClick={this.toggleMenu}>
+                            <li><NavLink to='/' exact >Home</NavLink></li>
+                            <li><NavLink to='/about' exact >About</NavLink></li>
+                            <li><NavLink to='/contact' exact >Contact</NavLink></li>
+                        </ul>
+                        <ul className="NavbarSocialIcon">
+                            <li><a href=""><i className="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i className="fab fa-twitter"></i></a></li>
+                            <li><a href=""><i className="fab fa-github"></i></a></li>
+                        </ul>
+                    </nav>
 
-            <Route path="/" exact component={Home} />
-            <Route path="/about" exact component={About} />
-            <Route path="/contact" exact component={Contact} />
+                </div>
+
+                    <div className='PageContent'>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/about" exact component={About} />
+                        <Route path="/contact" exact component={Contact} />
+                    </div>
+
+                
+
             </Aux >
         );
     }
